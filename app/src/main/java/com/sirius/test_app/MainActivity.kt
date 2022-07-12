3392dfa6d98516187c6f1b63aeb038ad2abc5a0c
SergeyRusak <rusak200281@gmail.com>
2022-07-13 05:49:39 +0800

    add more design elements + scrollable reviews

--- app/src/main/java/com/sirius/test_app/MainActivity.kt
+++ app/src/main/java/com/sirius/test_app/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.sirius.test_app
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.RatingBar
-import android.widget.TextView
+import android.text.Layout
+import android.view.LayoutInflater
+import android.widget.*
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ResourceAsColor")
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,6 +32,12 @@ class MainActivity : AppCompatActivity() {
         val app_logo_imageView = findViewById<ImageView>(R.id.app_logo);
         val app_preview_imageView = findViewById<ImageView>(R.id.app_preview);
 
+        val tag_list = findViewById<LinearLayout>(R.id.tag_list);
+        val review_list = findViewById<LinearLayout>(R.id.review_list);
+
+        val linf = LayoutInflater.from(this);
+
+
 
 
         //insert info in view
@@ -33,9 +45,31 @@ class MainActivity : AppCompatActivity() {
         app_description_textView.setText(data.description);
         app_rating_text_textView.setText(data.rating.toString());
         review_count_textView.setText(data.gradeCnt);
-        review_count2_textView.setText(data.gradeCnt);
+        review_count2_textView.setText(data.gradeCnt+" reviews");
         app_rating_ratingBar.rating = data.rating;
         app_rating2_ratingBar.rating = data.rating;
+
+        tag_list.removeAllViews();
+
+        for (item in data.tags){
+           val tv = TextView(this);
+            tv.setTextAppearance(R.style.tagText);
+            tv.setBackgroundResource(R.drawable.textview_style)
+            tv.text = item;
+           tag_list.addView(tv);
+        }
+        review_list.removeAllViews();
+        for (item in data.reviews){
+            val review = linf.inflate(R.layout.review, null);
+            review.findViewById<TextView>(R.id.name).setText(item.userName);
+            review.findViewById<TextView>(R.id.date).setText(item.date);
+            review.findViewById<TextView>(R.id.comment).setText(item.message);
+
+            review_list.addView(review);
+
+        }
+
+
 
 
 
