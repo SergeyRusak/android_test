0fe4f46dec47759aae93e4fc1f611000d404bbef
SergeyRusak <rusak200281@gmail.com>
2022-07-12 23:46:48 +0800

    structure of main_view done + insert information from DataModel.kt image links in DataModel.kt were changed for easily access

--- app/src/main/java/com/sirius/test_app/DataModel.kt
+++ app/src/main/java/com/sirius/test_app/DataModel.kt
@@ -1,26 +1,26 @@
 package com.sirius.test_app
 
 class DataModel(
-    val image: String = "https://ibb.co/37KzF03",
-    val logo: String = "https://ibb.co/GpbZg5t",
+    val image: String = "https://i.ibb.co/g3YVWD2/img-background.png",
+    val logo: String = "https://i.ibb.co/Pjf2B69/img-logo.png",
     val name: String = "DoTA 2",
     val rating: Float = 4.9f,
     val gradeCnt: String = "70M",
     val tags: List<String> = listOf("MOBA", "MULTIPLAYER", "STRATEGY"),
     val videos: List<VideoModel> = listOf(
-        VideoModel(id = "video_1", image = "https://ibb.co/q7dyBVH"),
-        VideoModel(id = "video_2", image = "https://ibb.co/S3GymhJ")
+        VideoModel(id = "video_1", image = "https://i.ibb.co/6rmnYLK/img-video.png"),
+        VideoModel(id = "video_2", image = "https://i.ibb.co/z4Dx2yN/img-video-2.png")
     ),
     val description: String = "Dota 2 is a multiplayer online battle arena (MOBA) game which has two teams of five players compete to collectively destroy a large structure defended by the opposing team known as the \"Ancient\", whilst defending their own.",
     val reviews: List<ReviewModel> = listOf(
         ReviewModel(
-            userImage = "https://ibb.co/WcJMjSw",
+            userImage = "https://i.ibb.co/55HSwqy/img-user-1.png",
             userName = "Auguste Conte",
             date = "February 14, 2019",
             message = "“Once you start to learn its secrets, there’s a wild and exciting variety of play here that’s unmatched, even by its peers.”"
         ),
         ReviewModel(
-            userImage = "https://ibb.co/p1q4QZr",
+            userImage = "https://i.ibb.co/j8t3zGZ/img-user-2.png",
             userName = "Jang Marcelino",
             date = "February 14, 2019",
             message =
--- app/src/main/java/com/sirius/test_app/MainActivity.kt
+++ app/src/main/java/com/sirius/test_app/MainActivity.kt
@@ -2,10 +2,43 @@ package com.sirius.test_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // get info about application
+        val data = DataModel();
+
+        //get id from view
+        val app_name_textView = findViewById<TextView>(R.id.app_name);
+        val app_description_textView = findViewById<TextView>(R.id.app_description);
+        val review_count_textView = findViewById<TextView>(R.id.review_count);
+        val review_count2_textView = findViewById<TextView>(R.id.review_count4);
+        val app_rating_text_textView = findViewById<TextView>(R.id.app_rating_text);
+
+        val app_rating_ratingBar = findViewById<RatingBar>(R.id.app_rating);
+        val app_rating2_ratingBar = findViewById<RatingBar>(R.id.app_rating4);
+
+        val app_logo_imageView = findViewById<ImageView>(R.id.app_logo);
+        val app_preview_imageView = findViewById<ImageView>(R.id.app_preview);
+
+
+
+        //insert info in view
+        app_name_textView.setText(data.name);
+        app_description_textView.setText(data.description);
+        app_rating_text_textView.setText(data.rating.toString());
+        review_count_textView.setText(data.gradeCnt);
+        review_count2_textView.setText(data.gradeCnt);
+        app_rating_ratingBar.rating = data.rating;
+        app_rating2_ratingBar.rating = data.rating;
+
+
+
+
     }
 }
